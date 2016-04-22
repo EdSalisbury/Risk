c9ef1abfe92d804dbce6a5a17823d3145634328e
Ed Salisbury <esalisbury@ebay.com>
2016-04-22 12:55:29 -0700

    Added update_score() and update_exposure()

--- risk.py
+++ risk.py
@@ -59,6 +59,29 @@ def move_list(player_id, min):
                     moves.append((terr_id, adj_id))
     return moves
 
+
+def update_exposure(state):
+    for player_id in state['players']:
+        for terr_id in state['players'][player_id]['territories']:
+            enemy_troops = 0
+            for adj_terr_id in adjacencies[terr_id]:
+                if adj_terr_id not in state['players'][player_id]['territories']:
+                    enemy_troops += state['troops'][adj_terr_id]
+        state['exposure'][terr_id] = enemy_troops - state['troops'][terr_id]
+
+
+def update_score(state):
+    for player_id in state['players']:
+        state['players'][player_id]['score'] = 0
+        for continent in state['players'][player_id]['continents']:
+            state['players'][player_id]['score'] += continents[continent]['value'] * 50
+
+        for territory in state['players'][player_id]['territories']:
+            state['players'][player_id]['score'] += territories[territory]['value'] * 5
+            state['players'][player_id]['score'] -= state['exposure'][territory]
+
+
+
 random.seed()
 
 continents = load_json_data('continents')
@@ -177,3 +200,27 @@ win_chance_min = 0.7
 # Determine move list
 for player in players:
     print move_list(player['id'], win_chance_min)
+
+# Generate a new state based on a move
+# Need a function to get board position, given a passed state
+# Get pre-move board state, save for that tier - add to a graph
+# How to creat a tree -- use a series of dicts
+# Index each state - something like level#-move#
+# Have a dict of states with that index being the key
+# Add exposure ratings to score (subtract?)
+
+# Multiple phases:
+#   Placement
+#   Attack
+#   Fortify - move from 1 territory to an adjacent (leaving one behind)
+
+# Thus, in the most common case (3 dice vs 2 dice), the attacker loses an average of 0.921 armies
+#  to the defender's 1.079, which is a ratio of about 6 to 7 (corrected in FAQ 5.6, previous versions gave 5 to 6 here).
+
+# Create a board state dict
+
+state = dict()
+# state['players'][player_id] = {"territories": [2, 4, 6], "continents": [1, 3, 5], "score": 0, "troops": 0}
+# state['troops'] = [5, 2, 5, 4, 2... ]
+# state['exposure'] = [0, 2, 5, 3, ...]
+
